558529c67d00a9eb23af853011709f67e463668d
izak <isac@eyeem.com>
2015-11-25 12:37:18 +0100

    remove bogus views

--- the_deck/views.py
+++ the_deck/views.py
@@ -6,18 +6,4 @@ import logging
 logger = logging.getLogger(__name__)
 
 def home(request):
-    res = rexecute.apply_async(args=("ls -lah", ["10.0.4.10", "10.0.4.68"],))
-    result = res.get()
-
-    ctx = {
-        "status": res.status
-    }
-
-    if res.successful():
-        ctx['results'] = []
-        for host, response in result.items():
-            ctx['results'].append((host, response.splitlines()))
-    else:
-        ctx['error'] == str(result)
-
-    return render(request, "home.html", ctx)
+    return render(request, "home.html", {})
