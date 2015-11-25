61f4063baa48f2bf627300985375ec1f38495e23
izak <isac@eyeem.com>
2015-11-25 17:14:16 +0100

    hack manager task import into celery autodiscover

--- the_deck/models/__init__.py
+++ the_deck/models/__init__.py
@@ -7,3 +7,6 @@ from task_run_result import TaskRunResult
 from task_runner import TaskRunner
 from task_set import TaskSet
 from user_profile import UserProfile
+
+#HACK!  Avoids circular imports in model definitions and registers via celery imports
+from the_deck.manager import run_until_complete
